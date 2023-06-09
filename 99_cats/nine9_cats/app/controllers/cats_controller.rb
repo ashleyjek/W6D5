e76a87115fd8cbfad3176f513e759566e15936fc
artemplv <artemplv@gmail.com>
2023-06-09 17:11:39 -0400

    add index and show views

---
+++ 99_cats/nine9_cats/app/controllers/cats_controller.rb
@@ -0,0 +1,16 @@
+class CatsController < ApplicationController
+  def index
+    @cats = Cat.all.order(:id)
+    render :index
+  end
+
+  def show
+    @cat = Cat.find(params[:id])
+    render :show
+  end
+
+  private
+  def cat_params
+    params.require(:cat).permit(:birth_date, :color, :sex, :name, :description)
+  end
+end
--- 99_cats/nine9_cats/app/models/cat.rb
+++ 99_cats/nine9_cats/app/models/cat.rb
@@ -1,9 +1,18 @@
+require "action_view"
+
 class Cat < ApplicationRecord
+    include ActionView::Helpers::DateHelper
+
     CAT_COLORS = %w(white black gray ginger purple)
     validate :birth_date_cannot_be_future
     validates :name, presence: true
     validates :color, inclusion: { in: CAT_COLORS }, presence: true
     validates :sex, inclusion: { in: %w(F M) }
+
+    def age
+        # time_ago_in_words(DateTime.now.year - birth_date.year)
+        DateTime.now.year - birth_date.year
+    end
 
     private
     def birth_date_cannot_be_future
@@ -11,9 +20,4 @@ class Cat < ApplicationRecord
             errors.add(:birth_date, 'can\'t be in the future')
         end
     end
-
-  
-
-
-    
 end
