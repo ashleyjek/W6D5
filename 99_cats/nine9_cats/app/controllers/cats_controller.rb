23242efd3e4206b0aa9f84e8bd4255fbd54f1f3f
ashley <ashleykimca@gmail.com>
2023-06-09 17:31:51 -0400

    Added new.html

--- 99_cats/nine9_cats/app/controllers/cats_controller.rb
+++ 99_cats/nine9_cats/app/controllers/cats_controller.rb
@@ -9,6 +9,14 @@ class CatsController < ApplicationController
     render :show
   end
 
+  def new
+    render :new
+  end
+
+  def create
+    
+  end
+
   private
   def cat_params
     params.require(:cat).permit(:birth_date, :color, :sex, :name, :description)
